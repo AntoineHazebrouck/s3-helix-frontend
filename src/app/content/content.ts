05d11bcdec6adb2d1664674f988ec8e45e2f94e5
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-09-29 17:18:56 +0200

    display hard coded tweets

--- src/app/content/content.ts
+++ src/app/content/content.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   imports: [],
   templateUrl: './content.html',
-  styleUrl: './content.css'
+  styleUrl: './content.css',
 })
 export class Content {
-
+  protected readonly tweets: string[] = ['Je fais caca', 'sqdsqdsqd', 'tototoaorrf'];
 }

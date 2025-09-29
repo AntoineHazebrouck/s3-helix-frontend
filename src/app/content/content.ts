0e2633792dda982af9a3c38ac1ff46447d36194a
Antoine Hazebrouck <antoine.hazebrouck5@gmail.com>
2025-09-29 22:30:03 +0200

    content update

--- src/app/content/content.ts
+++ src/app/content/content.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Tweet } from '../../model/tweet';
 
 @Component({
   selector: 'app-content',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './content.html',
   styleUrl: './content.css',
 })
 export class Content {
+  form = new FormGroup({
+    text: new FormControl(''),
+  });
   protected readonly tweets: Tweet[] = [
     new Tweet('Antoine HAZEBROUCK', 'Je fais caca'),
     new Tweet('Axel ELIAS', 'Tu fais caca'),
   ];
+
+  onSubmit() {
+    console.log(this.form.value.text);
+  }
 }
